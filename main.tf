2cb7f7414394674027f96360b5671ed8f0705974
nikkaushal <nikkaushal@gmial.com>
2026-03-24 15:49:27 -0400

    terraform test

--- main.tf
+++ main.tf
@@ -29,22 +29,22 @@ variable "components" {
   }
 }
 
-# resource "null_resource" "ansible" {
+resource "null_resource" "ansible" {
 
-#   depends_on = [aws_route53_record.dns]
+  depends_on = [aws_route53_record.dns]
 
-#   for_each = var.components
-#   provisioner "remote-exec" {
-#     connection {
-#       type = "ssh"
-#       host = aws_instance.instance[each.key].public_ip
-#       user = "ec2-user"
-#       password = "DevOps321"
-#     }
-#     inline = [
-#       "sudo labauto ansible",
-#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb88/wmp-ansible-v4.git main.yml -e env=dev -e COMPONENT=${each.key}"
-#     ]
-#   }
-# }
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = aws_instance.instance[each.key].public_ip
+      user = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/nikkaushal/wmp-terraform-v1.git main.yml -e env=dev -e COMPONENT=${each.key}"
+    ]
+  }
+}
 
